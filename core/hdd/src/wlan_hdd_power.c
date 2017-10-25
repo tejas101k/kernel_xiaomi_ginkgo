OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Reset suspended flag during recovery
When WLAN driver goes thru recovery, it's no longer suspended. All
threads are woken up, and FW is also reset during the recovery.

CRs-Fixed: 2110156
Change-Id: I4f586919c0a25345c981149d79c707fbfd1c347e

core/hdd/src/wlan_hdd_power.c
@@ -1471,6 +1471,7 @@ QDF_STATUS hdd_wlan_shutdown(void)
 	if (true == pHddCtx->isMcThreadSuspended) {
 		complete(&cds_sched_context->ResumeMcEvent);
 		pHddCtx->isMcThreadSuspended = false;
+		pHddCtx->isWiphySuspended = false;
 	}
 #ifdef QCA_CONFIG_SMP
 	if (true == pHddCtx->is_ol_rx_thread_suspended) {

tejas101k/kernel_xiaomi_ginkgo

core/hdd/src/wlan_hdd_power.c
@@ -1288,6 +1288,7 @@ QDF_STATUS hdd_wlan_shutdown(void)
 	if (hdd_ctx->is_scheduler_suspended) {
 		scheduler_resume();
 		hdd_ctx->is_scheduler_suspended = false;
+		hdd_ctx->isWiphySuspended = false;
 	}
 #ifdef QCA_CONFIG_SMP
 	if (true == hdd_ctx->is_ol_rx_thread_suspended) {