8b24b85256d8cc6dbe1263f6bed49dc23b498e29
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-23 14:09:14 -0700

    kernel: Boost DDR bus when zygote-forked process becomes top app
    Boost to the max for 1 second whenever the top app changes, which
    improves app launch speeds and addresses jitter when switching between
    apps. A check to make sure that the top-app's parent is zygote ensures
    that a user-facing app is indeed what's added to the top app task group,
    since app processes are forked from zygote.
    
    Change-Id: Ia3650af54d356de335832c4c5e2576705fe09da1
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -13,6 +13,8 @@
 #include <linux/delayacct.h>
 #include <linux/pid_namespace.h>
 #include <linux/cgroupstats.h>
+#include <linux/binfmts.h>
+#include <linux/devfreq_boost.h>
 
 #include <trace/events/cgroup.h>
 
@@ -537,6 +539,13 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 		goto out_finish;
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
+
+	/* This covers boosting for app launches and app transitions */
+	if (!ret && !threadgroup &&
+	    !strcmp(of->kn->parent->name, "top-app") &&
+	    task_is_zygote(task)) {
+		devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 1000);
+	}
 
 out_finish:
 	cgroup_procs_write_finish(task);
