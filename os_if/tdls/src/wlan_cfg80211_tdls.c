bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Fill the pmf info for TDLS peer
Fill the pfm info of the TDLS peer before sending the peer assoc
command to FW.

Change-Id: I4e336c345c0fb8f063157b3e3a780efa777f1a74
CRs-Fixed: 2535832

os_if/tdls/src/wlan_cfg80211_tdls.c
@@ -418,6 +418,10 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 	if (params->ht_capa || params->vht_capa ||
 	    (params->sta_flags_set & BIT(NL80211_STA_FLAG_WME)))
 		req_info->is_qos_wmm_sta = true;
+	if (params->sta_flags_set & BIT(NL80211_STA_FLAG_MFP)) {
+		osif_debug("TDLS peer pmf capable");
+		req_info->is_pmf = 1;
+	}
 }
 
 int wlan_cfg80211_tdls_update_peer(struct wlan_objmgr_vdev *vdev,

tejas101k/kernel_xiaomi_ginkgo

os_if/tdls/src/wlan_cfg80211_tdls.c
@@ -421,6 +421,10 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 	if (params->ht_capa || params->vht_capa ||
 	    (params->sta_flags_set & BIT(NL80211_STA_FLAG_WME)))
 		req_info->is_qos_wmm_sta = true;
+	if (params->sta_flags_set & BIT(NL80211_STA_FLAG_MFP)) {
+		cfg80211_debug("TDLS peer pmf capable");
+		req_info->is_pmf = 1;
+	}
 }
 
 int wlan_cfg80211_tdls_update_peer(struct wlan_objmgr_vdev *vdev,