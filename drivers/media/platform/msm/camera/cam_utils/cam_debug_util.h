0ea44c01f8a9ae6e3523e9f956b83842fc522de8
Yaroslav Furman <yaro330@gmail.com>
2020-05-14 13:32:44 +0300

    msm: camera: properly kill debug logging
    This version shaves even more bytes (20-80kb) from final image
    and doesn't produce thousands of warnings when compiling with GCC.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>

--- drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
+++ drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
@@ -53,6 +53,9 @@
 #define CAM_IR_LED     (1 << 27)
 #define STR_BUFFER_MAX_LENGTH  1024
 
+/* Kill debug logging from camera */
+#define CAM_DEBUG_LOGGING 0
+
 /*
  *  cam_debug_log()
  *
@@ -71,6 +74,7 @@ static inline void cam_debug_log(unsigned int module_id, const char *func,
 {
 }
 
+#if CAM_DEBUG_LOGGING
 /*
  * cam_get_module_name()
  *
@@ -163,6 +167,22 @@ static inline const char *cam_get_module_name(unsigned int module_id)
  */
 #define CAM_ERR_RATE_LIMIT(__module, fmt, args...)                 \
 	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+
+#else /* CAM_DEBUG_LOGGING */
+#define CAM_ERR(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+#define CAM_WARN(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+#define CAM_INFO(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+#define CAM_INFO_RATE_LIMIT(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+#define CAM_DBG(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+#define CAM_ERR_RATE_LIMIT(__module, fmt, args...) \
+	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+
+#endif /* CAM_DEBUG_LOGGING */
 
 /*
  * CAM_ERR_RATE_LIMIT_CUSTOM
