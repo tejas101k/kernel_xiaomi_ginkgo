bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Add support to set/reset BLM suspend state
Once host sends WMI_WOW_ENABLE_CMDID then host components must not send
any WMI command to FW. In suspend state if host sends WMI command then FW
crashes.
Hence add support to set/reset suspend state of Blacklist manager.
Thus Blacklist manager checks the suspend state and then only sends WMI
command to FW.

Change-Id: I2581d38a485c4f930a6e2dfb951c0fdc2306fa3b
CRs-Fixed: 2765404

components/blacklist_mgr/core/src/wlan_blm_core.c
@@ -790,13 +790,69 @@ static void blm_fill_reject_list(qdf_list_t *reject_db_list,
 }
 
 #if defined(WLAN_FEATURE_ROAM_OFFLOAD)
+void blm_update_reject_ap_list_to_fw(struct wlan_objmgr_psoc *psoc)
+{
+	struct blm_config *cfg;
+	struct wlan_objmgr_pdev *pdev;
+	struct blm_pdev_priv_obj *blm_ctx;
+	struct blm_psoc_priv_obj *blm_psoc_obj;
+
+	blm_psoc_obj = blm_get_psoc_obj(psoc);
+	if (!blm_psoc_obj) {
+		blm_err("BLM psoc obj NULL");
+		return;
+	}
+
+	pdev = wlan_objmgr_get_pdev_by_id(psoc, blm_psoc_obj->pdev_id,
+					  WLAN_MLME_CM_ID);
+	if (!pdev) {
+		blm_err("pdev obj NULL");
+		return;
+	}
+
+	blm_ctx = blm_get_pdev_obj(pdev);
+	if (!blm_ctx) {
+		blm_err("BLM pdev obj NULL");
+		goto end;
+	}
+
+	cfg = &blm_psoc_obj->blm_cfg;
+	blm_send_reject_ap_list_to_fw(pdev, &blm_ctx->reject_ap_list, cfg);
+
+end:
+	wlan_objmgr_pdev_release_ref(pdev, WLAN_MLME_CM_ID);
+}
+
+static void blm_store_pdevid_in_blm_psocpriv(struct wlan_objmgr_pdev *pdev)
+{
+	struct blm_psoc_priv_obj *blm_psoc_obj;
+
+	blm_psoc_obj = blm_get_psoc_obj(wlan_pdev_get_psoc(pdev));
+
+	if (!blm_psoc_obj) {
+		blm_err("BLM psoc obj NULL");
+		return;
+	}
+
+	blm_psoc_obj->pdev_id = pdev->pdev_objmgr.wlan_pdev_id;
+}
+
 void
 blm_send_reject_ap_list_to_fw(struct wlan_objmgr_pdev *pdev,
 			      qdf_list_t *reject_db_list,
 			      struct blm_config *cfg)
 {
 	QDF_STATUS status;
+	bool is_blm_suspended;
 	struct reject_ap_params reject_params = {0};
+
+	ucfg_blm_psoc_get_suspended(wlan_pdev_get_psoc(pdev),
+				    &is_blm_suspended);
+	if (is_blm_suspended) {
+		blm_store_pdevid_in_blm_psocpriv(pdev);
+		blm_debug("Failed to send reject AP list to FW as BLM is suspended");
+		return;
+	}
 
 	reject_params.bssid_list =
 			qdf_mem_malloc(sizeof(*reject_params.bssid_list) *

tejas101k/kernel_xiaomi_ginkgo

components/blacklist_mgr/core/src/wlan_blm_core.c
@@ -839,13 +839,69 @@ static void blm_fill_reject_list(qdf_list_t *reject_db_list,
 	}
 }
 
+void blm_update_reject_ap_list_to_fw(struct wlan_objmgr_psoc *psoc)
+{
+	struct blm_config *cfg;
+	struct wlan_objmgr_pdev *pdev;
+	struct blm_pdev_priv_obj *blm_ctx;
+	struct blm_psoc_priv_obj *blm_psoc_obj;
+
+	blm_psoc_obj = blm_get_psoc_obj(psoc);
+	if (!blm_psoc_obj) {
+		blm_err("BLM psoc obj NULL");
+		return;
+	}
+
+	pdev = wlan_objmgr_get_pdev_by_id(psoc, blm_psoc_obj->pdev_id,
+					  WLAN_MLME_NB_ID);
+	if (!pdev) {
+		blm_err("pdev obj NULL");
+		return;
+	}
+
+	blm_ctx = blm_get_pdev_obj(pdev);
+	if (!blm_ctx) {
+		blm_err("BLM pdev obj NULL");
+		goto end;
+	}
+
+	cfg = &blm_psoc_obj->blm_cfg;
+	blm_send_reject_ap_list_to_fw(pdev, &blm_ctx->reject_ap_list, cfg);
+
+end:
+	wlan_objmgr_pdev_release_ref(pdev, WLAN_MLME_NB_ID);
+}
+
+static void blm_store_pdevid_in_blm_psocpriv(struct wlan_objmgr_pdev *pdev)
+{
+	struct blm_psoc_priv_obj *blm_psoc_obj;
+
+	blm_psoc_obj = blm_get_psoc_obj(wlan_pdev_get_psoc(pdev));
+
+	if (!blm_psoc_obj) {
+		blm_err("BLM psoc obj NULL");
+		return;
+	}
+
+	blm_psoc_obj->pdev_id = pdev->pdev_objmgr.wlan_pdev_id;
+}
+
 void
 blm_send_reject_ap_list_to_fw(struct wlan_objmgr_pdev *pdev,
 			      qdf_list_t *reject_db_list,
 			      struct blm_config *cfg)
 {
 	QDF_STATUS status;
+	bool is_blm_suspended;
 	struct reject_ap_params reject_params = {0};
+
+	ucfg_blm_psoc_get_suspended(wlan_pdev_get_psoc(pdev),
+				    &is_blm_suspended);
+	if (is_blm_suspended) {
+		blm_store_pdevid_in_blm_psocpriv(pdev);
+		blm_debug("Failed to send reject AP list to FW as BLM is suspended");
+		return;
+	}
 
 	reject_params.bssid_list =
 			qdf_mem_malloc(sizeof(*reject_params.bssid_list) *