ebe839d4a44850ff05b2c4e55254424fb2ac2437
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-01 00:18:11 +0530

    cpufreq: Prevent userspace boosts when battery saver is active

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -32,6 +32,7 @@
 #include <linux/tick.h>
 #include <linux/sched/topology.h>
 #include <linux/sched/sysctl.h>
+#include <linux/battery_saver.h>
 
 #include <trace/events/power.h>
 
@@ -725,6 +726,10 @@ static ssize_t store_##file_name					\
 {									\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
+									\
+	if (&policy->object == &policy->min &&				\
+			is_battery_saver_on())				\
+		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
