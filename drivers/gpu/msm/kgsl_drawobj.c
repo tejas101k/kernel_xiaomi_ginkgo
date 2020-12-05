40a8039dea87190e1842f7d8f4ba6bdbe3ccf0db
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-05 13:34:48 -0800

    msm: kgsl: Remove sync fence names
    Generating a sync fence name by allocating memory dynamically and using
    scnprintf in a hot path results in excessive CPU time wasted on unneeded
    debug info. Remove the name generation entirely to cut down CPU waste in
    the GPU's rendering hot path.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>

--- drivers/gpu/msm/kgsl_drawobj.c
+++ drivers/gpu/msm/kgsl_drawobj.c
@@ -43,20 +43,6 @@
 static struct kmem_cache *memobjs_cache;
 static struct kmem_cache *sparseobjs_cache;
 
-#ifdef CONFIG_FENCE_DEBUG
-static void free_fence_names(struct kgsl_drawobj_sync *syncobj)
-{
-	unsigned int i;
-
-	for (i = 0; i < syncobj->numsyncs; i++) {
-		struct kgsl_drawobj_sync_event *event = &syncobj->synclist[i];
-
-		if (event->type == KGSL_CMD_SYNCPOINT_TYPE_FENCE)
-			kfree(event->info.fences);
-	}
-}
-#endif
-
 void kgsl_drawobj_destroy_object(struct kref *kref)
 {
 	struct kgsl_drawobj *drawobj = container_of(kref,
@@ -68,9 +54,6 @@ void kgsl_drawobj_destroy_object(struct kref *kref)
 	switch (drawobj->type) {
 	case SYNCOBJ_TYPE:
 		syncobj = SYNCOBJ(drawobj);
-#ifdef CONFIG_FENCE_DEBUG
-		free_fence_names(syncobj);
-#endif
 		kfree(syncobj->synclist);
 		kfree(syncobj);
 		break;
@@ -111,14 +94,6 @@ void kgsl_dump_syncpoints(struct kgsl_device *device,
 			break;
 		}
 		case KGSL_CMD_SYNCPOINT_TYPE_FENCE: {
-#ifdef CONFIG_FENCE_DEBUG
-			int j;
-			struct event_fence_info *info = &event->info;
-
-			for (j = 0; j < info->num_fences; j++)
-				dev_err(device->dev, "[%d]  fence: %s\n",
-					i, info->fences[j].name);
-#endif
 			break;
 		}
 		}
@@ -166,14 +141,6 @@ static void syncobj_timer(unsigned long data)
 				i, event->context->id, event->timestamp);
 			break;
 		case KGSL_CMD_SYNCPOINT_TYPE_FENCE: {
-#ifdef CONFIG_FENCE_DEBUG
-			int j;
-			struct event_fence_info *info = &event->info;
-
-			for (j = 0; j < info->num_fences; j++)
-				dev_err(device->dev, "       [%u] FENCE %s\n",
-					i, info->fences[j].name);
-#endif
 			break;
 		}
 		}
@@ -359,13 +326,6 @@ EXPORT_SYMBOL(kgsl_drawobj_destroy);
 static bool drawobj_sync_fence_func(void *priv)
 {
 	struct kgsl_drawobj_sync_event *event = priv;
-#ifdef CONFIG_FENCE_DEBUG
-	int i;
-
-	for (i = 0; i < event->info.num_fences; i++)
-		trace_syncpoint_fence_expire(event->syncobj,
-			event->info.fences[i].name);
-#endif
 
 	/*
 	 * Only call kgsl_drawobj_put() if it's not marked for cancellation
@@ -408,8 +368,7 @@ static int drawobj_add_sync_fence(struct kgsl_device *device,
 	set_bit(event->id, &syncobj->pending);
 
 	event->handle = kgsl_sync_fence_async_wait(sync->fd,
-				drawobj_sync_fence_func, event,
-				&event->info);
+				drawobj_sync_fence_func, event);
 
 	if (IS_ERR_OR_NULL(event->handle)) {
 		int ret = PTR_ERR(event->handle);
@@ -428,11 +387,6 @@ static int drawobj_add_sync_fence(struct kgsl_device *device,
 
 		return ret;
 	}
-
-#ifdef CONFIG_FENCE_DEBUG
-	for (i = 0; i < event->info.num_fences; i++)
-		trace_syncpoint_fence(syncobj, event->info.fences[i].name);
-#endif
 
 	return 0;
 }
