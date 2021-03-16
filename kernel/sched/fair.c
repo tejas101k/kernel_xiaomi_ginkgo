65f56709a7957ab1816a14a279ef4323c78c5c52
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-16 15:13:53 +0530

    sched/fair: Revert few changes from google
     * seems to have increased jitter
    
    Revert "sched/fair: Placement optimization for heavy load"
    
    This reverts commit 39430de0e71b7e98d7e1dfb89c827eb868ddd8c7.
    
    Revert "sched/fair: Check if mid capacity cpu exists"
    
    This reverts commit 09bee6ec4907d6eb36c4b705ba84738a53ed76c6.
    
    Revert "sched/fair: Skip cpu if task does not fit in"
    
    This reverts commit 241b80b261c12c3f24f09167bbb5e852f43b34be.
    
    Revert "sched/fair: Move cpu_is_in_target_set definition"
    
    This reverts commit d4f86427d6a846c78d4abeb94a71dccbf83a6655.
    
    Revert "sched/fair: Avoid little cpus due to sync, prev bias"
    
    This reverts commit 86f8c5dead12b0e3b40a984fbc1c43775f051813.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5844,34 +5844,11 @@ static unsigned long __cpu_norm_util(unsigned long util, unsigned long capacity)
 	return (util << SCHED_CAPACITY_SHIFT)/capacity;
 }
 
-/*
- * Check whether cpu is in the fastest set of cpu's that p should run on.
- * If p is boosted, prefer that p runs on a faster cpu; otherwise, allow p
- * to run on any cpu.
- */
-static inline bool
-cpu_is_in_target_set(struct task_struct *p, int cpu)
-{
-	struct root_domain *rd = cpu_rq(cpu)->rd;
-
-	int first_cpu, next_usable_cpu;
-
-	if (schedtune_task_boost(p) && p->prio <= DEFAULT_PRIO) {
-		first_cpu = rd->mid_cap_orig_cpu != -1 ? rd->mid_cap_orig_cpu :
-			    rd->max_cap_orig_cpu;
-	} else {
-		first_cpu = rd->min_cap_orig_cpu;
-	}
-
-	next_usable_cpu = cpumask_next(first_cpu - 1, &p->cpus_allowed);
-	return cpu >= next_usable_cpu || next_usable_cpu >= nr_cpu_ids;
-}
-
 static inline bool
 bias_to_this_cpu(struct task_struct *p, int cpu, int start_cpu)
 {
 	bool base_test = cpumask_test_cpu(cpu, &p->cpus_allowed) &&
-			cpu_active(cpu) && cpu_is_in_target_set(p, cpu);
+			cpu_active(cpu);
 	bool start_cap_test = (capacity_orig_of(cpu) >=
 					capacity_orig_of(start_cpu));
 
@@ -7753,15 +7730,11 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * The target CPU can be already at a capacity level higher
 			 * than the one required to boost the task.
 			 * However, if the task prefers idle cpu and that
-			 * cpu is idle, skip this check, but still need to make
-			 * sure the task fits in that cpu after considering
-			 * capacity margin.
+			 * cpu is idle, skip this check.
 			 */
 			new_util = max(min_util, new_util);
-			if ((!(prefer_idle && idle_cpu(i)) &&
-			     new_util > capacity_orig) ||
-			    (is_min_capacity_cpu(i) &&
-			     !task_fits_capacity(p, capacity_orig, i)))
+			if (!(prefer_idle && idle_cpu(i))
+				&& new_util > capacity_orig)
 				continue;
 
 			/*
@@ -7841,13 +7814,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					continue;
 				}
 				if (best_idle_cpu != -1)
-					continue;
-
-				/*
-				 * Skip searching for active CPU for tasks have
-				 * high priority & prefer_high_cap.
-				 */
-				if (prefer_idle && p->prio <= DEFAULT_PRIO)
 					continue;
 
 				/*
@@ -8075,9 +8041,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 	if (target_cpu == -1 && most_spare_cap_cpu != -1 &&
 		/* ensure we use active cpu for active migration */
-		!(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)) &&
-		/* do not pick an overutilized most_spare_cap_cpu */
-		!cpu_overutilized(most_spare_cap_cpu))
+		!(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)))
 		target_cpu = most_spare_cap_cpu;
 
 	trace_sched_find_best_target(p, prefer_idle, min_util, start_cpu,
