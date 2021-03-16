b1e067a8f3d4c38050405217124600f8c4bf0494
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-16 15:08:15 +0530

    techpack/audio: Remove unused tas2562 codec driver
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- techpack/audio/asoc/sm6150.c
+++ techpack/audio/asoc/sm6150.c
@@ -7565,12 +7565,9 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 #if defined(CONFIG_SND_SOC_MAX98937)
 		.codec_name = "max98927",
 		.codec_dai_name = "max98927-aif1",
-//#elif defined(CONFIG_SND_SOC_TAS2562)	
-//		.codec_name     = "tas2562.0-004c",
-//		.codec_dai_name = "tas2562 ASI1",
 #else
 		.codec_name = "msm-stub-codec.1",
-              .codec_dai_name = "msm-stub-rx",
+		.codec_dai_name = "msm-stub-rx",
 #endif
 		.no_pcm = 1,
 		.dpcm_playback = 1,
@@ -7588,9 +7585,6 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 #if defined(CONFIG_SND_SOC_MAX98937)
 		.codec_name = "max98927",
 		.codec_dai_name = "max98927-aif1",
-//#elif defined(CONFIG_SND_SOC_TAS2562)	
-//		.codec_name     = "tas2562.0-004c",
-//		.codec_dai_name = "tas2562 ASI1",
 #else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-tx",
