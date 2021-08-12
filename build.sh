c5c400cf304e3ae092b35d8563a029d040c1f921
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-12 23:20:56 +0530

    build.sh: Use local AK3 repo when it exists
     * git cloning sometimes takes a while...
     * also exit with error code 1 when build fails

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@
 SECONDS=0 # builtin bash timer
 ZIPNAME="QuicksilveRV2-ginkgo-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$HOME/tc/proton-clang"
+AK3_DIR="$HOME/android/AnyKernel3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
@@ -39,14 +40,17 @@ make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as 
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3; then
-echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
 exit 1
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
+git checkout master &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
@@ -58,4 +62,5 @@ curl --upload-file $ZIPNAME http://transfer.sh/$ZIPNAME; echo
 fi
 else
 echo -e "\nCompilation failed!"
+exit 1
 fi
