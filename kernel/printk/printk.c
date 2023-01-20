839a62f700f692143852cf0617b1ea2afdeb897f
siimsek <malisimsek17@gmail.com>
2023-01-20 05:32:54 -0500

    printk: Silence logd logspam
    [  123.634939] logd: logdr: UID=1000 GID=1000 PID=9198 n tail=500 logMask=1d pid=0 start=0ns deadline=0ns
    
    Signed-off-by: siimsek <malisimsek17@gmail.com>
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -810,6 +810,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			if (strstr(line, "logd"))
+				return ret;
 		}
 	}
 
